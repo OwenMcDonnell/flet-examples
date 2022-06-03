28cceed54fdee9ef0912b6c3eb75d6ae5dcd2427
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2022-06-03 15:21:48 -0700

    Initial layout.

--- python/trello-clone/main.py
+++ python/trello-clone/main.py
@@ -22,6 +22,7 @@ from flet import (
     NavigationRail,
     NavigationRailDestination,
     VerticalDivider,
+    Divider,
     AppBar,
     PopupMenuButton,
     PopupMenuItem,
@@ -30,7 +31,9 @@ from flet import (
     border_radius,
     colors,
     icons,
-    padding
+    padding,
+    margin,
+    border
 )
 
 # trello app
@@ -43,63 +46,80 @@ boards = []
 
 def main(page: Page):
     page.title = "Flet Trello clone"
-    page.theme_mode = "dark"
-    page.bgcolor = colors.AMBER_800
+    # page.theme_mode = "dark"
+    page.bgcolor = colors.LIGHT_GREEN_400
+
+    def search_board(e):
+        "TODO"
+
     page.appbar = AppBar(
-        leading=Icon(icons.PALETTE),
-        leading_width=40,
-        title=Text("AppBar Example"),
+        leading=Icon(icons.GRID_GOLDENRATIO_ROUNDED),
+        leading_width=100,
+        title=Text("Trolli"),
         center_title=False,
-        bgcolor=colors.SURFACE_VARIANT,
+        toolbar_height=75,
+        bgcolor=colors.LIGHT_BLUE_ACCENT_700,
         actions=[
-            IconButton(icons.WB_SUNNY_OUTLINED),
-            IconButton(icons.FILTER_3),
-            PopupMenuButton(
-                items=[
-                    PopupMenuItem(text="Item 1"),
-                    PopupMenuItem(),  # divider
-                    PopupMenuItem(
-                        text="Checked item", checked=False
-                    ),
-                ]
+            Container(
+                content=Row(
+                    [
+                        TextField(hint_text="Search this board", border="none",
+                                  autofocus=False, on_submit=search_board, content_padding=padding.all(15), filled=False, suffix_icon=icons.SEARCH)
+                    ],
+                    alignment="spaceAround"
+                ),
+                bgcolor=colors.WHITE24,
+                margin=margin.all(15),
+                border=border.all(2, colors.WHITE),
+                border_radius=border_radius.all(30)
             ),
+            Container(
+                content=PopupMenuButton(
+                    items=[
+                        PopupMenuItem(text="Profile"),
+                        PopupMenuItem(),  # divider
+                        PopupMenuItem(
+                            text="Synchronize"
+                        )
+                    ]
+                ),
+                margin=margin.only(left=50, right=25)
+            )
         ],
     )
+
     sidebar = NavigationRail(
         destinations=[
             NavigationRailDestination(
 
                 padding=padding.all(5),
-                label_content=Container(content=Text("1st Board"), bgcolor=colors.WHITE12,
-                                        border_radius=border_radius.all(30), padding=5, expand=True)
+                # label_content=Row(
+                #     [Text("Empty Board")]
+                # )
+                label_content=Text("Empty Board"),
+                selected_icon=icons.CHEVRON_RIGHT_ROUNDED
             ),
             *boards
         ],
-        bgcolor=colors.WHITE24,
-        leading=Row(controls=[
-            Text("Add new board", text_align="center", weight="w500"),
-            IconButton(icon=icons.ADD, icon_size=20,
-                       icon_color="blue")
-        ]
+        bgcolor=colors.LIGHT_GREEN_700,
+        leading=Container(
+            content=Row(
+                controls=[
+                    TextButton("Add new board", icon=icons.ADD),
+                    # Text("Add new board", text_align="center", weight="w500"),
+                    # IconButton(icon=icons.ADD, icon_size=20)
+                ],
+                # alignment="spaceEvenly",
+                # expand=True
+            ),
+            border_radius=border_radius.all(35),
+            bgcolor=colors.WHITE12,
+            padding=padding.all(0),
+            margin=margin.all(0)
         ),
+        selected_index=0,
         extended=True
     )
-    # sidebar = Column(
-    #     [
-    #         Container(
-    #             expand=True,
-    #             content=Row(controls=[
-    #                 Text("Add new board", text_align="center", weight="w500"),
-    #                 IconButton(icon=icons.ADD, icon_size=20,
-    #                            icon_color="blue")
-    #             ]),
-    #             bgcolor=colors.BLUE_GREY_300,
-    #             padding=padding.all(5)
-    #         )
-    #     ],
-    #     width=200,
-    #     expand=True
-    # )
 
     def addBoard():
         "TODO"
